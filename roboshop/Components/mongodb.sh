009e81efc7c1ffe5be0776584a15dc036aebadec
swapnildss456 <swapnildss456@gmail.com>
2022-10-27 20:12:38 +0530

    mongod

--- roboshop/Components/mongodb.sh
+++ roboshop/Components/mongodb.sh
@@ -4,7 +4,7 @@ set -e
 Component=mongod
 logfile=/tmp/$Component.log
 Update() {
-    if [$1 -eq 0]; then
+    if [ $1 -eq 0 ]; then
         echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m Failure \e[0m"
