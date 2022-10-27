c307c7fc8248e1f6c761e139ca6e16d71c81a75e
swapnildss456 <swapnildss456@gmail.com>
2022-10-27 20:11:43 +0530

    mongod

--- roboshop/Components/mongodb.sh
+++ roboshop/Components/mongodb.sh
@@ -4,7 +4,7 @@ set -e
 Component=mongod
 logfile=/tmp/$Component.log
 Update() {
-    if [$1 -eq 0] ; then
+    if [$1 -eq 0]; then
         echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m Failure \e[0m"
@@ -16,7 +16,7 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans
 Update $?
 
 echo -n "Install MongoDB"
-yum install -y mongodb-org
+yum install -y mongodb-org &>> $logfile
 Update $?
 
 echo -n "Enable Mongo Service"
