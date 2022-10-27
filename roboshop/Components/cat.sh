41f72298d11ba03829537570965a92de8a7d164e
swapnildss456 <swapnildss456@gmail.com>
2022-10-27 21:15:19 +0530

    cat.sh

--- roboshop/Components/cat.sh
+++ roboshop/Components/cat.sh
@@ -30,6 +30,10 @@ Update $?
 
 echo -n "Install the code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip" &>> $logfile
+Update $?
+
+echo -n "Unzip the code"
+
 cd /home/roboshop
 unzip /tmp/catalogue.zip &>> $logfile
 mv catalogue-main catalogue
