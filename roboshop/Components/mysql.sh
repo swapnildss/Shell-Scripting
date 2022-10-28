754f07be06691f02603e07d6bd4769fdf3ec8452
swapnildss456 <swapnildss456@gmail.com>
2022-10-28 17:24:56 +0530

    mysql

--- roboshop/Components/mysql.sh
+++ roboshop/Components/mysql.sh
@@ -27,7 +27,7 @@ Update $?
 
 echo -n "Get the Passowrd"
 DB_PD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
-echo -e "\e[34m $DB_PD \e[0m"
+echo -n $DB_PD
 Update $?
 
 echo -n "MYSQL Setup"
